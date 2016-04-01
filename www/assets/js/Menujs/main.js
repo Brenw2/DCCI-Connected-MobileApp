a80c7f57b9d1db1a8f9b2dcdd5234ed0b647ce76
Kooper <klsmith6@bsu.edu>
2016-04-01 00:07:31 -0400

    Fixed background with full blur features and no graphical artifacts from blurring.

--- www/assets/js/Menujs/main.js
+++ www/assets/js/Menujs/main.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function () {
     if ($('.cd-stretchy-nav').length > 0) {
         var stretchyNavs = $('.cd-stretchy-nav');
         var page = $('.appPage');
+        var body = $('customBackground');
 
         stretchyNavs.each(function () {
             var stretchyNav = $(this)
@@ -11,6 +12,7 @@ jQuery(document).ready(function () {
                 event.preventDefault();
                 stretchyNav.toggleClass('nav-is-visible');
                 page.toggleClass('forgroundDarkness');
+                body.toggleClass('customBlur');
                 stretchyNavs.toggleClass('stectchyNavSizeOpen');
 
             });
