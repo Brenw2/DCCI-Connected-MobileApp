9f51b68064f76d9318d2db988b96d69a7cf333b2
Kooper <klsmith6@bsu.edu>
2016-04-02 00:04:45 -0400

    Switched to production server.

--- www/assets/js/prayerwall.js
+++ www/assets/js/prayerwall.js
@@ -9,8 +9,8 @@ function getPage() {
 	return path.substring(path.lastIndexOf('/') + 1);
 }
 
-var host = "prayerwall-test.azurewebsites.net";
-var url = "http://prayerwall-test.azurewebsites.net/api/PrayerRequest/";
+var host = "dccmuncieprayerwallapi.azurewebsites.net";
+var url = "https://" + host + "/api/PrayerRequest/";
 
 function setupGetRequest() {
 	var request = new XMLHttpRequest();
@@ -35,22 +35,26 @@ function populateWallFromJSON(json) {
 	wall.innerHTML = html;
 }
 
+const GUID = "@guid";
 const PRECANT = "@precant";
 const TIMEAGO = "@timeago";
 const PRAYER = "@prayer";
 const PRAYER_TEMPLATE = //
-"<div>" + //
-"	<span style='font-weight: bold;'>" + PRECANT + "</span>" + //
-"	&nbsp;-&nbsp;" + //
-"	<span style='font-style: italic;'>" + TIMEAGO + "</span>" + //
-"	<br/>" + //
-"	<div style='background: rgba(144, 144, 144, 0.075); padding: 3px;'>" + //
-"		" + PRAYER + //
-"	</div>" + //
-"</div>";
+"<a href='prayerView.html?guid=" + GUID + "'>" + //
+"   <div class='prayer'>" + //
+"	    <span style='font-weight: bold;'>" + PRECANT + "</span>" + //
+"	    &nbsp;-&nbsp;" + //
+"		<span style='font-style: italic;'>" + TIMEAGO + "</span>" + //
+"		<br/>" + //
+"		<div style='background: rgba(144, 144, 144, 0.075); padding: 3px;'>" + //
+"			" + PRAYER + //
+"		</div>" + //
+"   </div>" + //
+"</a";
 
 function getHtml(prayer) {
 	return PRAYER_TEMPLATE//
+	.replace(GUID, prayer.Id)//
 	.replace(PRECANT, prayer.PrayerRequesterName)//
 	.replace(TIMEAGO, prayer.TimeStamp)//
 	.replace(PRAYER, prayer.PrayerRequestMessage);
@@ -80,33 +84,18 @@ function setupPostRequest() {
 	request.send(params);
 }
 
-/*
- POST http://localhost:55558/api/PrayerRequest/ HTTP/1.1
- User-Agent: Fiddler
- Host: localhost:55558
- Content-type: application/json
- Content-Length: 117
-
- {
- "PrayerRequesterName":"Tristan",
- "TimeStamp":"2009-04-12T20:44:55",
- "PrayerRequestMessage":"Ryan is the best"
- }
- HTTP/1.1 201 Created
- Cache-Control: no-cache
- Pragma: no-cache
- Content-Type: application/json; charset=utf-8
- Expires: -1
- Location: http://localhost:55558/api/PrayerRequest(guid'd96e2c98-f348-427c-9417-b5c5dcd1a205')
- Server: Microsoft-IIS/10.0
- DataServiceVersion: 3.0
- X-AspNet-Version: 4.0.30319
- X-SourceFiles: =?UTF-8?B?QzpcVXNlcnNcdHJpc3RfMDAwXE9uZURyaXZlXFNvZnR3YXJlRGV2ZWxvcG1lbnRcVmlzdWFsU3R1ZGlvMjAxNVxQcm9qZWN0c1xQcmF5ZXJXYWxsXFByYXllcldhbGwuV2ViQXBpXGFwaVxQcmF5ZXJSZXF1ZXN0XA==?=
- X-Powered-By: ASP.NET
- Date: Wed, 09 Mar 2016 03:48:11 GMT
- Content-Length: 238
-
- {
- "odata.metadata":"http://localhost:55558/api/$metadata#PrayerRequest/@Element","Id":"d96e2c98-f348-427c-9417-b5c5dcd1a205","PrayerRequesterName":"Tristan","TimeStamp":"2009-04-12T20:44:55","PrayerRequestMessage":"Ryan is the best"
- }
- */
+function getParameterByName(name, url) {
+	if (!url) {
+		url = window.location.href;
+	}
+	name = name.replace(/[\[\]]/g, "\\$&");
+	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i"),
+	    results = regex.exec(url);
+	if (!results) {
+		return null;
+	}
+	if (!results[2]) {
+		return '';
+	}
+	return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
